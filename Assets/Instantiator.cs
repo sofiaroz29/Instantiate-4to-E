0f1e1dac502e8c21e981357dd23ba7f2ccd3a22f
sofiaroz29 <101563492+sofiaroz29@users.noreply.github.com>
2022-05-10 15:48:47 -0300

    instantiator through datainput

--- Assets/Instantiator.cs
+++ Assets/Instantiator.cs
@@ -1,28 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Instantiator : MonoBehaviour
 {
     public GameObject objectToClone;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-        
-    }
+    int cloneAmount;
+    public Text txtUserCloneAmount;
 
-    // Update is called once per frame
-    void Update()
+    public void MultiInstantiate()
     {
         
-    }
+        if (txtUserCloneAmount.text!= "")
+        {
+            cloneAmount = int.Parse(txtUserCloneAmount.text);
+        }
 
-    public void TripleInstantiate()
-    {
         int counter = 0;
-        while (counter < 3)
+        while (counter < cloneAmount)
         {
             Instantiate(objectToClone);
             counter++;
