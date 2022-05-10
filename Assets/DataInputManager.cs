142661976d44d0017ef1cdd0caa6fc738b6f9166
sofiaroz29 <101563492+sofiaroz29@users.noreply.github.com>
2022-05-10 15:31:44 -0300

    Input Field

---
+++ Assets/DataInputManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DataInputManager : MonoBehaviour
+{
+    public Text txtUserName;
+
+    public void ShowGreetings()
+    {
+        Debug.Log("Hola " + txtUserName.text);
+    }
+}
